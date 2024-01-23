4862025103dfa8b858f3a9437d92f179ec602d42
Aryeh Zax <aryehzax@gmail.com>
2024-01-23 00:03:15 -0800

    new Sunday events

--- src/main/event/ScheduleInfo.java
+++ src/main/event/ScheduleInfo.java
@@ -116,13 +116,16 @@ public class ScheduleInfo {
 			
 			Sunday.add(new Event(0, VINTAGE, EventType.CHALLENGE));
 			Sunday.add(new Event(1, LIMITED, EventType.CHALLENGE_32));
-			Sunday.add(new Event(4, MODERN, EventType.CHALLENGE));
+			Sunday.add(new Event(4, MODERN, EventType.CHALLENGE_96));
 			Sunday.add(new Event(6, PIONEER, EventType.CHALLENGE));
 			Sunday.add(new Event(8, LEGACY, EventType.CHALLENGE));
-			Sunday.add(new Event(10, PAUPER, EventType.CHALLENGE_32));
+			Sunday.add(new Event(9, MODERN, EventType.PRELIM));
+			Sunday.add(new Event(10, PAUPER, EventType.CHALLENGE));
 			Sunday.add(new Event(12, LIMITED, EventType.CHALLENGE_32));
-			Sunday.add(new Event(14, STANDARD, EventType.CHALLENGE));
+			Sunday.add(new Event(13, PIONEER, EventType.CHALLENGE_32));
+			Sunday.add(new Event(14, STANDARD, EventType.CHALLENGE_96));
 			Sunday.add(new Event(16, LIMITED));
+			Sunday.add(new Event(18, MODERN, EventType.CHALLENGE_32));
 			
 			return Sunday;
 		}
